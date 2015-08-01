657991c7498923e43c5736ad117d33017a05aa64
shibinraj2005 <freelancersvk@yahoo.com>
2015-08-01 22:13:37 +0530

    added to the repo

---
+++ LoanManagementSystem/Models/LoanDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LoanManagementSystem.Models
+{
+    public class LoanDBContext:DbContext
+    {
+        public LoanDBContext():base("name=LoanDBContext")
+    {
+
+    }
+        public DbSet<UserGroup> Usergroup { get; set; }
+        public DbSet<User> User { get; set; }
+        
+        
+        
+    }
+}
---
+++ LoanManagementSystem/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LoanManagementSystem.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [MaxLength(10)]
+        public string Code { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        
+        public int UserGroupID { get; set; }
+        [ForeignKey("UserGroupID")]
+        public virtual UserGroup UserGroup { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
