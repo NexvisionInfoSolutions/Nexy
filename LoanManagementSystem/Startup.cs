11a927be19c60c6f1da263474bd80a26fd689ebf
digeeshs <digeeshs@gmail.com>
2015-08-01 12:36:21 +0530

    Loan Management System - Base MVC

---
+++ LoanManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoanManagementSystem.Startup))]
+namespace LoanManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
