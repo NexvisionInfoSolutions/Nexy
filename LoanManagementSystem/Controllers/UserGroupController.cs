ba3a2cab852d12a265380084c863c9b989bbd9e5
shibinraj2005 <freelancersvk@yahoo.com>
2015-08-01 22:14:04 +0530

    added to the repo

---
+++ LoanManagementSystem/Controllers/UserGroupController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LoanManagementSystem.Models;
+
+namespace LoanManagementSystem.Controllers
+{
+    public class UserGroupController : Controller
+    {
+        private LoanDBContext db = new LoanDBContext();
+
+        // GET: /UserGroup/
+        public ActionResult Index()
+        {
+            return View(db.Usergroup.ToList());
+        }
+
+        // GET: /UserGroup/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserGroup usergroup = db.Usergroup.Find(id);
+            if (usergroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usergroup);
+        }
+
+        // GET: /UserGroup/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /UserGroup/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Code,Description")] UserGroup usergroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usergroup.Add(usergroup);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usergroup);
+        }
+
+        // GET: /UserGroup/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserGroup usergroup = db.Usergroup.Find(id);
+            if (usergroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usergroup);
+        }
+
+        // POST: /UserGroup/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Code,Description")] UserGroup usergroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usergroup).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usergroup);
+        }
+
+        // GET: /UserGroup/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserGroup usergroup = db.Usergroup.Find(id);
+            if (usergroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usergroup);
+        }
+
+        // POST: /UserGroup/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserGroup usergroup = db.Usergroup.Find(id);
+            db.Usergroup.Remove(usergroup);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
