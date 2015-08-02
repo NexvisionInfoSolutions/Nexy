dc37df26a4a75506bf6edcdf7a8aca6c50f6a548
digeeshs <digeeshs@gmail.com>
2015-08-02 08:46:33 +0530

    Business layer added

--- Business/Base/bfBase.cs
+++ Business/Base/bfBase.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Business.Base
 {
-    class bfBase
+    class bfBase : DbContext
     {
+        public bfBase(System.Data.Common.DbConnection dbConnection, Boolean OwnsConnection) : base(dbConnection, OwnsConnection) { }
     }
 }
