5358d287ea19d7b75b4c497d65768715f89eba0f
digeeshs <digeeshs@gmail.com>
2015-08-09 08:49:00 +0530

    Design incorporated

--- LoanManagementSystem/App_Start/BundleConfig.cs
+++ LoanManagementSystem/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace LoanManagementSystem
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jQuery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -23,8 +23,15 @@ namespace LoanManagementSystem
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/layout").Include(
+                      "~/Scripts/layout.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/ionicons.min.css",
+                      "~/Content/Layout.css",
+                      "~/Content/skins/skin-green.css",
                       "~/Content/site.css"));
         }
     }
